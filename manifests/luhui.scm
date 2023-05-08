8900b9a7c92b3f7917f1ddecbc4ec6012a8df878
Lu Hui <luhux76@gmail.com>
2023-05-08 22:11:59 +0800

    manifests: remove some unused package

--- manifests/luhui.scm
+++ manifests/luhui.scm
@@ -15,26 +15,23 @@
 
 (define-public %luhui-packages
   (list bash coreutils findutils grep sed diffutils patch gawk
-        tar gzip bzip2 xz e2fsprogs btrfs-progs dosfstools pciutils
-        usbutils util-linux kmod eudev less mandoc info-reader nvi
-        bash-completion kbd inetutils iproute wget curl iw glibc-locales
-        wireless-tools tmux picocom openssh dropbear man-pages
-        nss-certs emacs-next-pgtk emacs-company emacs-lsp-mode
-        emacs-lsp-ui emacs-rime emacs-telega fontconfig emacs-geiser
-        font-gnu-unifont font-terminus font-google-noto-emoji
-        weston foot icecat ungoogled-chromium/wayland w3m links
-        qtwayland imv pinentry-tty gnupg openssl password-store
-        git gnu-make rtl-sdr gqrx dump1090 psmisc htop bmon iftop
-	procps gdb gcc-toolchain cscope indent ncurses fastboot
-	guile-3.0-latest gkermit bvi abootimg binwalk adb blanket
-	singularity (list glibc "static") linux-libre-headers
+        tar gzip bzip2 xz util-linux less mandoc info-reader nvi
+        bash-completion wget curl iw glibc-locales wireless-tools
+	tmux picocom openssh dropbear man-pages nss-certs
+	emacs-next-pgtk emacs-company emacs-lsp-mode emacs-lsp-ui
+	emacs-rime emacs-telega fontconfig emacs-geiser font-gnu-unifont
+	font-terminus font-google-noto-emoji weston foot icecat
+	ungoogled-chromium w3m links pinentry-tty gnupg
+	openssl password-store git gnu-make rtl-sdr gqrx dump1090
+	psmisc htop bmon iftop procps gdb gcc-toolchain cscope indent
+	ncurses fastboot guile-3.0-latest gkermit bvi abootimg binwalk
+	adb blanket (list glibc "static") linux-libre-headers
 	emacs-geiser-guile virt-manager strace ltrace perf ccache
 	dbus file graphviz squashfs-tools alsa-utils ffmpeg lrzsz
 	openixcard grim wl-clipboard patchelf emacs-paredit
 	emacs-yasnippet emacs-yasnippet-snippets emacs-magit ccls
-	emacs-ccls bear cmake proxychains-ng adwaita-icon-theme
-	python-extract-dtb cutter radare2 blisp openocd xfel
-	ripgrep emacs-ripgrep fzf emacs-fzf neofetch tango-icon-theme
-        ek eksw ckermit screen qtbase))
+	emacs-ccls cmake proxychains-ng python-extract-dtb cutter
+	radare2 blisp openocd xfel ripgrep emacs-ripgrep fzf emacs-fzf
+	neofetch ek eksw ckermit screen bear))
 
 (packages->manifest %luhui-packages)
