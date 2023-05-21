2fdc96b73668b0d391c0cf86000b5f1931425d41
Lu Hui <luhux76@gmail.com>
2023-05-21 17:44:31 +0800

    home/home.scm: add alias for ustardict

--- home/home.scm
+++ home/home.scm
@@ -13,6 +13,11 @@
    (service home-bash-service-type
             (home-bash-configuration
              (guix-defaults? #t)
+	     (aliases
+	      '(("ustardict" . (string-append "ustardict "
+					      "~/.guix-home/profile/share"
+					      "/stardict/"
+					      "stardict-ecdict-2.4.2."))))
              (bashrc (list (local-file ".bashrc" "bashrc")))))
 
    (simple-service 'my-environment-variables
