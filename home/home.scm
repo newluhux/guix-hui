03c651e4d414e20443cd5466f8ba935b4ac5dbe8
Lu Hui <luhux76@gmail.com>
2023-05-21 15:02:05 +0800

    home: use home manager

---
+++ home/home.scm
@@ -0,0 +1,30 @@
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu services)
+             (guix gexp))
+
+(load "../manifest/luhui.scm")
+
+(home-environment
+ (packages %luhui-packages)
+ (services
+  (list
+   (service home-bash-service-type
+            (home-bash-configuration
+             (guix-defaults? #t)
+             (bashrc (list (local-file ".bashrc" "bashrc")))))
+
+   (simple-service 'my-environment-variables
+                   home-environment-variables-service-type
+                   `(("PAGER" . "less")
+                     ("EDITOR" . "emacsclient")))
+
+   (simple-service 'my-config-file
+                   home-files-service-type
+                   `(("emacs" ,(local-file ".emacs" "emacs"))
+                     ("Xdefaults" ,(local-file ".Xdefaults" "Xdefaults"))
+                     ("cwmrc" ,(local-file ".cwmrc" "cwmrc"))
+                     ("xinitrc" ,(local-file ".xinitrc" "xinitrc"
+                                             #:recursive? #t)))))))
+
