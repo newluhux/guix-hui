ef213bbaf15d7d08d8f2e9c2c199d63ddb70c774
Lu Hui <luhux76@gmail.com>
2023-05-31 22:18:05 +0800

    manifest/connect.scm: iperf3 test net speed

--- manifest/connect.scm
+++ manifest/connect.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles))
-(use-package-modules ssh terminals admin screen tmux)
+(use-package-modules ssh terminals admin screen tmux networking)
 
 (load "../packages/embedded.scm")
 
 (define-public %connect-packages
   (list
-   openssh picocom lrzsz ek eksw screen tmux gkermit ukermit ckermit))
+   openssh picocom lrzsz ek eksw screen tmux gkermit ukermit ckermit iperf))
 
 (packages->manifest %connect-packages)
