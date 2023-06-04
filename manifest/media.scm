0a23520d7788b146b6ddd74a1b8cefeb6453cb44
Lu Hui <luhux76@gmail.com>
2023-06-04 14:43:07 +0800

    manifest/media.scm: add k3b

--- manifest/media.scm
+++ manifest/media.scm
@@ -1,10 +1,10 @@
 (use-modules (gnu) (guix profiles))
-(use-package-modules image-viewers xdisorg video linux)
+(use-package-modules image-viewers xdisorg video linux kde-multimedia)
 
 (load "../packages/telegram.scm")
 
 (define-public %media-packages
   (list
-   feh scrot ffmpeg alsa-utils tgs2png))
+   feh scrot ffmpeg alsa-utils tgs2png k3b))
 
 (packages->manifest %media-packages)
